43ce6d817bd9132080bbafbb05c637a707836c87
Raydom <raydom007@163.com>
2018-05-18 17:25:56 +0800

    个人中心效果完善

--- mobile/src/App.vue
+++ mobile/src/App.vue
@@ -32,7 +32,6 @@ export default {
 <style lang="scss">
 .main {
   padding-bottom: 2.2rem;
-  background: $bgColor;
 }
 
 footer {
--- mobile/src/pages/center.vue
+++ mobile/src/pages/center.vue
@@ -14,20 +14,23 @@
       </span>
     </div>
     <ul class="social-wrap">
-      <li>
-        <span>123</span>作品</li>
-      <li>
+      <li class="works">
+        <span>12</span>作品</li>
+      <li class="attention">
         <span>123</span>关注</li>
-      <li>
+      <li class="fans">
         <span>123</span>粉丝</li>
-      <li>
+      <li class="comment">
         <span>123</span>评论</li>
     </ul>
     <div class="menu-list">
-      <router-link class="list-item" to="">积分说明
+      <router-link class="list-item data" to="">个人资料
         <span></span>
       </router-link>
-      <router-link class="list-item" to="">设置
+      <router-link class="list-item m-integral" to="">积分说明
+        <span></span>
+      </router-link>
+      <router-link class="list-item setting" to="">设置
         <span></span>
       </router-link>
     </div>
@@ -68,12 +71,32 @@ export default {
 </script>
 
 <style lang="scss" scoped>
+@keyframes hue-animation {
+  0% {
+    -webkit-filter: hue-rotate(0deg);
+    filter: hue-rotate(0deg);
+  }
 
+  100% {
+    -webkit-filter: hue-rotate(120deg);
+    filter: hue-rotate(120deg);
+  }
+}
 .info-wrap {
   padding: 2rem 0.75rem 3rem;
-  background: linear-gradient(-45deg, #d849b3, #f04c7f);
   overflow: hidden;
   position: relative;
+  &:after {
+    background: linear-gradient(-45deg, nth($mainColor, 1), nth($mainColor, 2));
+    animation: hue-animation 10s infinite alternate linear;
+    content: "";
+    position: absolute;
+    width: 100%;
+    height: 100%;
+    top: 0;
+    left: 0;
+    z-index:-1;
+  }
   .avatar {
     width: 4rem;
     height: 4rem;
@@ -152,12 +175,38 @@ export default {
     color: nth($fontColor, 2);
     margin: 1rem 0;
     border-right: 1px solid $bgColor;
+    position: relative;
+    padding-top: 1.6rem;
+    &.works {
+      background: url(#{$comUrl}icon-works.png) no-repeat center 0;
+      background-size: 1.4rem auto;
+    }
+    &.attention {
+      background: url(#{$comUrl}icon-attention.png) no-repeat center 0;
+      background-size: 1.4rem auto;
+    }
+    &.fans {
+      background: url(#{$comUrl}icon-fans.png) no-repeat center 0;
+      background-size: 1.4rem auto;
+    }
+    &.comment {
+      background: url(#{$comUrl}icon-comment.png) no-repeat center 0;
+      background-size: 1.4rem auto;
+    }
     span {
       display: block;
+      position: absolute;
+      left: 2.4rem;
+      top: -0.1rem;
       text-align: center;
-      font-size: 0.8rem;
+      font-size: 0.5rem;
+      border: 0.05rem solid nth($mainColor, 1);
       color: nth($mainColor, 1);
-      padding: 0 0 0.3rem;
+      height: 0.7rem;
+      line-height: 0.7rem;
+      border-radius: 0.35rem;
+      background: #fff;
+      padding: 0 0.2rem;
     }
   }
 }
@@ -165,14 +214,31 @@ export default {
   padding-top: 0.5rem;
   background: $bgColor;
   .list-item {
-    background: #fff;
     display: flex;
     justify-content: space-between;
-    height: 2.2rem;
-    line-height: 2.2rem;
+    height: 2.5rem;
+    line-height: 2.5rem;
     font-size: 0.8rem;
-    padding: 0 0.75rem;
-    margin-bottom: 1px;
+    padding: 0 0.75rem 0 2rem;
+    border-bottom: 1px solid $bgColor;
+    &.data {
+      background: #fff url(#{$comUrl}icon-data.png) no-repeat 0.75rem center;
+      background-size: 1rem auto;
+    }
+    &.m-integral {
+      background: #fff url(#{$comUrl}icon-integral.png) no-repeat 0.75rem center;
+      background-size: 1rem auto;
+    }
+    &.setting {
+      background: #fff url(#{$comUrl}icon-setting.png) no-repeat 0.75rem center;
+      background-size: 1rem auto;
+    }
+    span {
+      width: 2.2rem;
+      line-height: 2.2rem;
+      background: url(#{$comUrl}menu-next.png) no-repeat right center;
+      background-size: 40% auto;
+    }
   }
 }
 </style>
